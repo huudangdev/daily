734fd563f86cdf0e61bbbfcc644760dd07c54a59
Hữu Đăng <huudang@Dangs-MBP.local>
2020-03-29 18:26:42 +0700

    Updates

---
+++ precache-manifest.94018505723c66609933630e8e740b41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e0bab7a0b0b63caecc3535750e6844",
+    "url": "/daily/index.html"
+  },
+  {
+    "revision": "748564513c023624e877",
+    "url": "/daily/static/css/main.28a3d0e5.chunk.css"
+  },
+  {
+    "revision": "762051f56881680bdfd4",
+    "url": "/daily/static/js/2.b4421f23.chunk.js"
+  },
+  {
+    "revision": "536558ae0d2c3676e46653cadf22447a",
+    "url": "/daily/static/js/2.b4421f23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748564513c023624e877",
+    "url": "/daily/static/js/main.35294fc9.chunk.js"
+  },
+  {
+    "revision": "1430211c9fc3659ab184",
+    "url": "/daily/static/js/runtime-main.08d46617.js"
+  }
+]);

40cdebef90fcfecb078f8000c06ddea20f9e7648
Hữu Đăng <huudang@Dangs-MBP.local>
2020-03-29 19:30:21 +0700

    fix bugs deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 import ReactCardFlip from 'react-card-flip'
 import data from './text.json'
@@ -10,7 +10,7 @@ const randomInt = (min, max) => {
 
 const initialFlipping = [false, false, false, false, false]
 
-class App extends React.Component {
+class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -40,7 +40,6 @@ class App extends React.Component {
     const tmp = numRan.map((value) => {
       return data[value]
     })
-    console.log(tmp)
     this.setState({ mockData: tmp })
   }
 
@@ -56,91 +55,89 @@ class App extends React.Component {
 
   render () {
     return (
-      <>
-        <Flexbox flexDirection='column' minHeight='100vh'>
-          <Flexbox element='header' height='100px'>
-            <div className='fixed-top'>
-              <nav className='navbar navbar-expand-lg navbar-dark mx-background-top-linear' style={{ backgroundColor: '#e3f2fd' }}>
-                <div className='container'>
-                  <a className='navbar-brand' href='index.html'><h6 className='g-color-white-opacity-0_8 g-font-size-13'> VỒ CÁ BU LA RI </h6></a>
-                  <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarResponsive' aria-controls='navbarResponsive' aria-expanded='false' aria-label='Toggle navigation'>
-                    <span className='navbar-toggler-icon' />
-                  </button>
-                  <div className='collapse navbar-collapse' id='navbarResponsive'>
+      <Flexbox flexDirection='column' minHeight='100vh'>
+        <Flexbox element='header' height='100px'>
+          <div className='fixed-top'>
+            <nav className='navbar navbar-expand-lg navbar-dark mx-background-top-linear' style={{ backgroundColor: '#e3f2fd' }}>
+              <div className='container'>
+                <a className='navbar-brand' href='/'><h6 className='g-color-white-opacity-0_8 g-font-size-13'> VỒ CÁ BU LA RI CỦA CAM </h6></a>
+                <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarResponsive' aria-controls='navbarResponsive' aria-expanded='false' aria-label='Toggle navigation'>
+                  <span className='navbar-toggler-icon' />
+                </button>
+                <div className='collapse navbar-collapse' id='navbarResponsive'>
 
-                    <ul className='navbar-nav ml-auto'>
+                  <ul className='navbar-nav ml-auto'>
 
-                      <li className='nav-item active'>
-                        <a className='nav-link' href='/'>Home
-                          <span className='sr-only'>(current)</span>
-                        </a>
-                      </li>
+                    <li className='nav-item active'>
+                      <a className='nav-link' href='/'>Home
+                        <span className='sr-only'>(current)</span>
+                      </a>
+                    </li>
 
-                      <li className='nav-item'>
-                        <a className='nav-link' href='#'>About</a>
-                      </li>
+                    <li className='nav-item'>
+                      <a className='nav-link' href='/'>About</a>
+                    </li>
 
-                      <li className='nav-item'>
-                        <a className='nav-link' href='#'>Blog</a>
-                      </li>
+                    <li className='nav-item'>
+                      <a className='nav-link' href='/'>Blog</a>
+                    </li>
 
-                      <li className='nav-item'>
-                        <a className='nav-link' href='#'>Contact</a>
-                      </li>
-                    </ul>
-                  </div>
+                    <li className='nav-item'>
+                      <a className='nav-link' href='/'>Contact</a>
+                    </li>
+                  </ul>
                 </div>
-              </nav>
-            </div>
-          </Flexbox>
-
-          <Flexbox flexGrow={1}>
-            <div className='container'>
-              <div className='outer'>
-                <div className='inner rotate'><h6 className='g-color-white-opacity-0_8 g-font-size-13'>S e e . m o r e ?. C l i c k . o n . t h e . c a r d</h6></div>
               </div>
-              <div className='row justify-content-md-center'>
-                {this.state.mockData.map((value, index) => {
-                  return (
-                    <div className='col col-md-4' key={index}>
-                      <ReactCardFlip
-                        isFlipped={this.state.isFlipped[index]}
-                        flipSpeedBackToFront={2}
-                        flipSpeedFrontToBack={2}
-                        key={index}
-                      >
-                        <div className='card text-center' style={{ width: 200, height: 220 }} onClick={() => this.handleClick(index)}>
-                          <div className='card-header'>
-                        W O R D {index + 1}
-                          </div>
-                          <div className='card-body'>
-                            <h3 className='g-color-white-opacity-0_8 g-font-size-13'>{value['Tiếng Anh']}</h3>
-                            <h6 className='card-subtitle mb-2 text-muted'>{value['Phiên âm']}</h6>
-                            <h6 className='blockquote-footer'>Type: <cite title='Source Title' style={{ textTransform: 'uppercase' }}>{value['Loại từ']}</cite></h6>
-                            {/* {value['Đồng nghĩa'] && <p className='card-text'><i>synonym:</i> {value['Đồng nghĩa']}</p>} */}
-                          </div>
-                        </div>
-
-                        <div className='card text-center' style={{ width: 200, height: 220 }} onClick={() => this.handleClick(index)}>
-                          <div className='card-header'>
-                        W O R D {index + 1}
-                          </div>
-                          <div className='card-body'>
-                            <h5 className='card-title'>{value['Tiếng Anh']}</h5>
-                            <h6 className='card-subtitle mb-2 text-muted'>{value['Phiên âm']}</h6>
-                            <p className='card-text'>{value['Tiếng Việt']}</p>
-                          </div>
-                        </div>
-                      </ReactCardFlip>
-                    </div>
-                  )
-                })}
-              </div>
-            </div>
-            <button type='button' class='btn btn-outline-info' href='/' onClick={() => this.initMockData()}>NEXT</button>
-          </Flexbox>
+            </nav>
+          </div>
         </Flexbox>
-      </>
+
+        <Flexbox flexGrow={1}>
+          <div className='container'>
+            <div className='outer'>
+              <div className='inner rotate'><h6 className='g-color-white-opacity-0_8 g-font-size-13'>S e e . m o r e ?. C l i c k . o n . t h e . c a r d</h6></div>
+            </div>
+            <div className='row justify-content-md-center'>
+              {this.state.mockData.map((value, index) => {
+                return (
+                  <div className='col col-md-4' key={index}>
+                    <ReactCardFlip
+                      isFlipped={this.state.isFlipped[index]}
+                      flipSpeedBackToFront={2}
+                      flipSpeedFrontToBack={2}
+                      key={index}
+                    >
+                      <div className='card text-center' style={{ width: 200, height: 220 }} onClick={() => this.handleClick(index)}>
+                        <div className='card-header'>
+                        W O R D {index + 1}
+                        </div>
+                        <div className='card-body'>
+                          <h3 className='g-color-white-opacity-0_8 g-font-size-13'>{value['Tiếng Anh']}</h3>
+                          <h6 className='card-subtitle mb-2 text-muted'>{value['Phiên âm']}</h6>
+                          <h6 className='blockquote-footer'>Type: <cite title='Source Title' style={{ textTransform: 'uppercase' }}>{value['Loại từ']}</cite></h6>
+                          {/* {value['Đồng nghĩa'] && <p className='card-text'><i>synonym:</i> {value['Đồng nghĩa']}</p>} */}
+                        </div>
+                      </div>
+
+                      <div className='card text-center' style={{ width: 200, height: 220 }} onClick={() => this.handleClick(index)}>
+                        <div className='card-header'>
+                        W O R D {index + 1}
+                        </div>
+                        <div className='card-body'>
+                          <h5 className='card-title'>{value['Tiếng Anh']}</h5>
+                          <h6 className='card-subtitle mb-2 text-muted'>{value['Phiên âm']}</h6>
+                          <p className='card-text'>{value['Tiếng Việt']}</p>
+                        </div>
+                      </div>
+                    </ReactCardFlip>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+          <button type='button' className='btn btn-outline-info' href='/' onClick={() => this.initMockData()}>NEXT</button>
+        </Flexbox>
+      </Flexbox>
     )
   }
 }
